33c45701eafd12acddf3a8189a2bfe62e9138aea
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-14 14:59:51 +0200

    Parametrized filename() test

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,25 +1,20 @@
 from helpers.file_handling import filename
 import pytest
 
-def test_filename():
+def test_filename_no_dict():
     dct = 5
     path = "none"
     with pytest.raises(Exception):
         filename(dct,path)
-    dct = {
-        "key": "value"
-    }
-    assert filename(dct,path) == "none/-"
-    dct = {
-        "exp_id": "exp",
-        }
-    assert filename(dct,path) == "none/exp-"
-    dct = {
-        "mon_id": "mon id",
-        }
-    assert filename(dct,path) == "none/-mon-id"
-    dct = {
-        "exp_id": "exp",
-        "mon_id": "mon id",
-        }
-    assert filename(dct,path) == "none/exp-mon-id"
+
+
+@pytest.mark.parametrize("dictionary, expected_result", [
+    ({"key": "value"}, "none/-"),
+    ({"exp_id": "exp"}, "none/exp-"),
+    ({"mon_id": "mon id"}, "none/-mon-id"),
+    ({"exp_id": "exp","mon_id": "mon id"}, "none/exp-mon-id")
+])
+
+def test_filename_dict(dictionary, expected_result):
+    path = "none"
+    assert filename(dictionary,path) == expected_result
