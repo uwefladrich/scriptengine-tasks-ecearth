d094726ba3f3a518fa46fb6d5301154cd311fd9f
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-14 11:35:30 +0200

    Fix Import Problem in simulated_years.py

--- scriptengine/tasks/processing/simulated_years.py
+++ scriptengine/tasks/processing/simulated_years.py
@@ -2,7 +2,7 @@
 
 from scriptengine.tasks.base import Task
 import yaml
-#from ....helpers import file_handling
+import helpers.file_handling as file_handling
 
 class SimulatedYears(Task):
     def __init__(self, parameters):
@@ -25,10 +25,10 @@ class SimulatedYears(Task):
         diagnostic = {
             "exp_id": self.exp_id,
             "mon_id": self.mon_id,
-            "description": self. description,
+            "description": self.description,
             "data": self.get_leg_number(),
         }
-        convert_to_yaml(diagnostic,self.dst)
+        file_handling.convert_to_yaml(diagnostic,self.dst)
         pass
 
     def get_leg_number(self):
@@ -41,24 +41,3 @@ class SimulatedYears(Task):
             leg_number = -1
         return leg_number
 
-
-def filename(diagnostic, destination):
-    """Creates the file name for a diagnostic dictionary."""
-    
-    if "exp_id" in diagnostic:
-        exp_id = diagnostic["exp_id"]
-    else:
-        exp_id = "exp_id-not-given"
-    
-    if "mon_id" in diagnostic:
-        mon_id = "-".join(diagnostic["mon_id"].split())
-    else:
-        mon_id = "mon_id-not-given"
-    
-    return f"{destination}/{exp_id}-{mon_id}"
-    
-
-def convert_to_yaml(diagnostic, destination):
-    """Converts a diagnostic dictionary to a YAML file."""
-    with open(f"{filename(diagnostic, destination)}.yml", 'w') as outfile:
-        yaml.dump(diagnostic, outfile, sort_keys=False)
