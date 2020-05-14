50ba0faa51d7390ae338e0784d88b406a547c822
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-14 15:45:12 +0200

    Added Test for convert_to_yaml()

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,5 +1,7 @@
-from helpers.file_handling import filename
+import helpers.file_handling as file_handling
 import pytest
+import yaml
+
 
 
 @pytest.mark.parametrize("dictionary, expected_result", [
@@ -7,9 +9,22 @@ import pytest
     ({"key": "value"}, "none/-"),
     ({"exp_id": "exp"}, "none/exp-"),
     ({"mon_id": "mon id"}, "none/-mon-id"),
-    ({"exp_id": "exp","mon_id": "mon id"}, "none/exp-mon-id")
+    ({"exp_id": "exp","mon_id": "mon id"}, "none/exp-mon-id"),
 ])
 
 def test_filename(dictionary, expected_result):
     path = "none"
-    assert filename(dictionary,path) == expected_result
+    assert file_handling.filename(dictionary,path) == expected_result
+
+@pytest.mark.parametrize("list_of_dicts", [
+    5,
+    {"key": "value"},
+    {"exp_id": "exp"},
+    {"mon_id": "mon id"},
+    {"exp_id": "exp","mon_id": "mon id"},
+])
+
+def test_convert_to_yaml(tmpdir, list_of_dicts):
+    file_handling.convert_to_yaml(list_of_dicts, str(tmpdir))
+    file = open(f"{file_handling.filename(list_of_dicts,str(tmpdir))}.yml")
+    assert list_of_dicts == yaml.load(file, Loader=yaml.FullLoader)
