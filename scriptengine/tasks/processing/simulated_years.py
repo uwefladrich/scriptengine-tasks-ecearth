90fef0e0550924d4abe76991d20572ff946f393f
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-14 11:24:48 +0200

    Fix Bug in __repr__

--- scriptengine/tasks/processing/simulated_years.py
+++ scriptengine/tasks/processing/simulated_years.py
@@ -15,7 +15,7 @@ class SimulatedYears(Task):
         self.mon_id = "simulated years"
         self.description = "Current leg number of EC-Earth run."
     
-    def __repr__():
+    def __repr__(self):
         return (
             f"{self.__class__.__name__}"
             f"({self.exp_id},{self.src},{self.dst})"
