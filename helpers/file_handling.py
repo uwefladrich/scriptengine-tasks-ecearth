256deac42eb1a2a383dd255c9465a51407baa8a9
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-18 16:19:06 +0200

    Make filename() More Shallow, Update Tests

--- helpers/file_handling.py
+++ helpers/file_handling.py
@@ -1,25 +1,24 @@
 import yaml
 import netCDF4
 
-def filename(diagnostic, destination):
+def filename(exp_id, mon_id, destination):
     """Creates the file name for a diagnostic dictionary."""
-    
-    try:
-        exp_id = diagnostic["exp_id"]
-    except (TypeError, KeyError):
-        exp_id = ""
-    
-    try:
-        mon_id = "-".join(diagnostic["mon_id"].split())
-    except (TypeError, KeyError):
-        mon_id = ""
-    
+    mon_id = "-".join(mon_id.split())
     return f"{destination}/{exp_id}-{mon_id}"
     
 
 def convert_to_yaml(diagnostic, destination):
     """Converts a diagnostic dictionary to a YAML file."""
-    with open(f"{filename(diagnostic, destination)}.yml", 'w') as outfile:
+    try:
+        exp_id = diagnostic["exp_id"]
+    except (TypeError, KeyError):
+        exp_id = ""
+    try:
+        mon_id = "-".join(diagnostic["mon_id"].split())
+    except (TypeError, KeyError):
+        mon_id = ""
+    
+    with open(f"{filename(exp_id, mon_id, destination)}.yml", 'w') as outfile:
         yaml.dump(diagnostic, outfile, sort_keys=False)
 
 
--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,30 +1,31 @@
+"""Tests for helpers/file_handling.py"""
+
 import helpers.file_handling as file_handling
 import pytest
 import yaml
 
 
 
-@pytest.mark.parametrize("dictionary, expected_result", [
-    (5, "none/-"),
-    ({"key": "value"}, "none/-"),
-    ({"exp_id": "exp"}, "none/exp-"),
-    ({"mon_id": "mon id"}, "none/-mon-id"),
-    ({"exp_id": "exp","mon_id": "mon id"}, "none/exp-mon-id"),
+@pytest.mark.parametrize("exp_id, mon_id, expected_result", [
+    ("", "", "none/-"),
+    ("exp", "", "none/exp-"),
+    ("", "mon id", "none/-mon-id"),
+    ("exp","mon id", "none/exp-mon-id"),
 ])
 
-def test_filename(dictionary, expected_result):
+def test_filename(exp_id, mon_id, expected_result):
     path = "none"
-    assert file_handling.filename(dictionary,path) == expected_result
+    assert file_handling.filename(exp_id, mon_id, path) == expected_result
 
-@pytest.mark.parametrize("list_of_dicts", [
-    5,
-    {"key": "value"},
-    {"exp_id": "exp"},
-    {"mon_id": "mon id"},
-    {"exp_id": "exp","mon_id": "mon id"},
+@pytest.mark.parametrize("list_of_dicts, expected_input", [
+    (5, ["", ""]),
+    ({"key": "value"}, ["", ""]),
+    ({"exp_id": "exp"}, ["exp", ""]),
+    ({"mon_id": "mon id"}, ["", "mon id"]),
+    ({"exp_id": "exp","mon_id": "mon id"}, ["exp","mon id"]),
 ])
 
-def test_convert_to_yaml(tmpdir, list_of_dicts):
+def test_convert_to_yaml(tmpdir, list_of_dicts, expected_input):
     file_handling.convert_to_yaml(list_of_dicts, str(tmpdir))
-    with open(f"{file_handling.filename(list_of_dicts,str(tmpdir))}.yml") as file:
+    with open(f"{file_handling.filename(*expected_input,str(tmpdir))}.yml") as file:
         assert list_of_dicts == yaml.load(file, Loader=yaml.FullLoader)
