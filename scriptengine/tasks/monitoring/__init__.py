07cebec714b39f0f3a901a2ea35dd6fda562b211
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-20 11:55:54 +0200

    New Task "WriteScalar", Rename SimulatedYears

--- scriptengine/tasks/monitoring/__init__.py
+++ scriptengine/tasks/monitoring/__init__.py
@@ -1,10 +1,12 @@
-from .simulated_years import SimulatedYears
+from .simulated_legs import SimulatedLegs
+from .write_scalar import WriteScalar
 from .markdown_output import MarkdownOutput
 from .global_average import GlobalAverage
 
 def task_loader_map():
     return {
-        'ec.mon.sim_years': SimulatedYears,
-        'ec.mon.markdown_report': MarkdownOutput,
-        'ec.mon.global_avg': GlobalAverage,
+        'ece.mon.sim_legs': SimulatedLegs,
+        'ece.mon.write_scalar': WriteScalar,
+        'ece.mon.markdown_report': MarkdownOutput,
+        'ece.mon.global_avg': GlobalAverage,
         }
---
+++ scriptengine/tasks/monitoring/write_scalar.py
@@ -0,0 +1,36 @@
+"""Processing Task that writes out a given scalar value."""
+
+from scriptengine.tasks.base import Task
+import yaml
+import os
+import helpers.file_handling as file_handling
+from scriptengine.jinja import render as j2render
+
+class WriteScalar(Task):
+    def __init__(self, parameters):
+        required = [
+            "name",
+            "value",
+            "dst",
+        ]
+        super().__init__(__name__, parameters, required_parameters=required)
+        self.mon_id = "write scalar"
+        #TODO: Description?
+    
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"({self.name},{self.value},{self.dst})"
+        )
+
+    def run(self, context):
+        self.mon_id = f"write scalar {j2render(self.name, context)}"
+        self.value = j2render(self.value, context)
+        self.dst = j2render(self.dst, context)
+
+        diagnostic = {
+            "mon_id": self.mon_id,
+            "data": self.value,
+        }
+
+        file_handling.convert_to_yaml(diagnostic,self.dst)
