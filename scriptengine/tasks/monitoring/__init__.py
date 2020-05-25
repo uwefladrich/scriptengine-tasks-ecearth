4c05f4947c66e617848a2c85c734485c12d48e3f
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-25 14:31:45 +0200

    Add Processing Task SimulatedYears

--- scriptengine/tasks/monitoring/__init__.py
+++ scriptengine/tasks/monitoring/__init__.py
@@ -1,4 +1,5 @@
 from .simulated_legs import SimulatedLegs
+from .simulated_years import SimulatedYears
 from .write_scalar import WriteScalar
 from .markdown_output import MarkdownOutput
 from .global_average import GlobalAverage
@@ -6,6 +7,7 @@ from .global_average import GlobalAverage
 def task_loader_map():
     return {
         'ece.mon.sim_legs': SimulatedLegs,
+        'ece.mon.sim_years': SimulatedYears,
         'ece.mon.write_scalar': WriteScalar,
         'ece.mon.markdown_report': MarkdownOutput,
         'ece.mon.global_avg': GlobalAverage,
---
+++ scriptengine/tasks/monitoring/simulated_years.py
@@ -0,0 +1,42 @@
+"""Processing Task that writes out the number of so far simulated years."""
+from .write_scalar import WriteScalar
+import os
+from scriptengine.jinja import render as j2render
+from dateutil.relativedelta import relativedelta
+import datetime
+
+class SimulatedYears(WriteScalar):
+    def __init__(self, parameters):
+        required = [
+            "dst",
+            "start",
+            "end",
+        ]
+        super(WriteScalar, self).__init__(__name__, parameters, required_parameters=required)
+        self.name = "simulated years"
+        self.description = "Current number of simulated years."
+    
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"({self.dst},{self.start},{self.end})"
+        )
+
+    def run(self, context):
+        format_str = "%Y-%m-%d"
+
+        dst = j2render(self.dst, context)
+        start = j2render(self.start, context)
+        end = j2render(self.end, context)
+        start = datetime.datetime.strptime(start, format_str)
+        end = datetime.datetime.strptime(end, format_str)
+        
+        value = relativedelta(end, start).years
+
+        self.save(
+            dst,
+            name=self.name,
+            description=self.description,
+            data=value,
+        )
+
