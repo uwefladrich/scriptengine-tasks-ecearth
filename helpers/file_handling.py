bb1b96f37a0e6c2102346a70d073921162652662
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-28 12:06:24 +0200

    Add cd Helper Class to Change Directory
    can get used in markdown_output

--- helpers/file_handling.py
+++ helpers/file_handling.py
@@ -3,6 +3,22 @@ import netCDF4
 
 """File handling functions. Not necessary anymore."""
 
+import os
+
+
+# Using https://stackoverflow.com/revisions/13197763/9
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
 def filename(mon_id, destination):
     """Creates the file name for a diagnostic."""
     mon_id = "-".join(mon_id.split())
