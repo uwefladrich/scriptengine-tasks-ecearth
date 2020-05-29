6bd651e0068f82ca6483eb9979708e500a1c46f0
Valentina Schueller <valentina.schueller@gmail.com>
2020-05-29 09:46:04 +0200

    Plot Title is now long_name
    removed section header if there are multiple plots for the same diagnostic

--- helpers/cube_plot.py
+++ helpers/cube_plot.py
@@ -4,25 +4,21 @@ import iris.quickplot as qplt
 import matplotlib.pyplot as plt
 import cftime
 
-def _title(cube_or_coord, with_units):
+def _title(name, units=None):
     """
     Create Plot/Axis Title from Iris cube/coordinate
 
     Slight variance on _title() in iris/quickplot.py.
     """
-
-    if isinstance(cube_or_coord, int) or cube_or_coord == None:
-        title = ""
-    else:
-        title = cube_or_coord.name().replace("_", " ").title()
-        units = cube_or_coord.units
-        if with_units and not (
-                units.is_unknown() or units.is_no_unit()
+    title = name.replace("_", " ").title()
+    if not (
+        units == None 
+        or units.is_unknown() 
+        or units.is_no_unit()
         ):
-            if qplt._use_symbol(units):
-                units = units.symbol
-            title += " / {}".format(units)
-
+        if qplt._use_symbol(units):
+            units = units.symbol
+        title += " / {}".format(units)
     return title
 
 def ts_plot(ts_cube):
@@ -43,6 +39,6 @@ def ts_plot(ts_cube):
     plt.plot(fmt_dates, ts_cube.data, marker='o')
     plt.xticks(fmt_dates, rotation=45)
     plt.tight_layout()
-    plt.xlabel(_title(time_coord, False))
-    plt.title(_title(ts_cube, False))
-    plt.ylabel(_title(ts_cube, True))
+    plt.title(_title(ts_cube.long_name))
+    plt.xlabel(_title(time_coord.name()))
+    plt.ylabel(_title(ts_cube.name(), ts_cube.units))
