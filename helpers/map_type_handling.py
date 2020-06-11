836ea98c828ffeb06ee8e5d1cbffe095143cf181
Valentina Schueller <valentina.schueller@gmail.com>
2020-06-11 12:30:07 +0200

    Suppress Iris Warnings in global_ocean_plot

--- helpers/map_type_handling.py
+++ helpers/map_type_handling.py
@@ -1,5 +1,7 @@
 """Module containing plot functions for different map types."""
 
+import warnings
+
 import iris
 import iris.quickplot as qplt
 import iris.plot as iplt
@@ -18,17 +20,28 @@ def function_mapper(map_type_string):
 def global_ocean_plot(cube, title=None, value_range=None, units=None):
     plt.figure(dpi=300)
     ax = plt.subplot(projection=ccrs.PlateCarree())
-    projected_cube, _ = iris.analysis.cartography.project(
-        cube, 
-        ccrs.PlateCarree(),
-        nx=800,
-        ny=400,
-        )
-    im = iplt.pcolormesh(
-        projected_cube,
-        axes=ax,
-        clim=value_range
-        )
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            action='ignore', 
+            message="Coordinate system of latitude and longitude ", 
+            category=UserWarning,
+            )
+        warnings.filterwarnings(
+            action='ignore', 
+            message="Coordinate 'projection_._coordinate' is", 
+            category=UserWarning,
+            )
+        projected_cube, _ = iris.analysis.cartography.project(
+            cube, 
+            ccrs.PlateCarree(),
+            nx=800,
+            ny=400,
+            )
+        im = iplt.pcolormesh(
+            projected_cube,
+            axes=ax,
+            clim=value_range
+            )
     bar = plt.colorbar(im, orientation='horizontal')
     bar.set_label(units)
     ax.set_title(title)
