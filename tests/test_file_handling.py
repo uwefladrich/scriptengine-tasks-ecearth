2478ee3bd0a86514eff041c873509021503265ca
Valentina Schueller <valentina.schueller@gmail.com>
2020-06-28 18:43:50 +0200

    New Tests

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,28 +1,37 @@
 """Tests for helpers/file_handling.py"""
 
-import helpers.file_handling as file_handling
+import os
+
 import pytest
-import yaml
+import numpy as np
+import iris
 
+import helpers.file_handling as file_handling
 
+def test_get_month_from_src():
+    path_list = [
+        "dummy_01.nc",
+        "dummy_02.nc",
+        ]
+    assert file_handling.get_month_from_src('02', path_list) == "dummy_02.nc"
+    pytest.raises(
+        FileNotFoundError,
+        file_handling.get_month_from_src,
+        '03',
+        path_list,
+        )
 
-@pytest.mark.parametrize("mon_id, expected_result", [
-    ("", "none/"),
-    ("mon id", "none/mon-id"),
-])
+def test_change_directory(tmpdir):
+    cwd = os.getcwd()
+    nwd = tmpdir.mkdir('temp')
+    with file_handling.ChangeDirectory(nwd):
+        assert os.getcwd() == nwd
+    assert os.getcwd() == cwd
 
-def test_filename(mon_id, expected_result):
-    path = "none"
-    assert file_handling.filename(mon_id, path) == expected_result
-
-@pytest.mark.parametrize("list_of_dicts, expected_input", [
-    (5, ""),
-    ({"key": "value"}, ""),
-    ({"mon_id": "mon id"}, "mon id"),
-])
-
-def test_convert_to_yaml(tmpdir, list_of_dicts, expected_input):
-    file_handling.convert_to_yaml(list_of_dicts, str(tmpdir))
-    path = file_handling.filename(expected_input,str(tmpdir))
-    with open(f"{path}.yml") as file:
-        assert list_of_dicts == yaml.load(file, Loader=yaml.FullLoader)
+def test_compute_spatial_weights(tmpdir, monkeypatch):
+    e1t_cube = iris.cube.Cube([1], var_name='e1t')
+    e2t_cube = iris.cube.Cube([2], var_name='e2t')
+    cube_list = iris.cube.CubeList([e1t_cube, e2t_cube])
+    domain_path = str(tmpdir + "/temp.nc")
+    iris.save(cube_list, domain_path)
+    assert file_handling.compute_spatial_weights(domain_path, (3,1,1)).all() == np.array([[[2,]],[[2,]],[[2,]]]).all()
