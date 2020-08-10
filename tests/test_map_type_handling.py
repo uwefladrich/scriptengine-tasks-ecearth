b4924b43b051ec6636ccda2d27d7d2e4f15f1228
Valentina Schueller <valentina.schueller@gmail.com>
2020-08-10 16:06:26 +0200

    Add Tests for Map Type Handling, grib_cf_additions.py

---
+++ tests/test_map_type_handling.py
@@ -0,0 +1,24 @@
+import pytest
+import iris
+import numpy as np
+import matplotlib.pyplot as plt
+
+import helpers.map_type_handling as mth
+
+def test_global_ocean_plot():
+    ocean_cube = iris.load_cube("./tests/testdata/tos-climatology.nc")
+    assert mth.global_ocean_plot(ocean_cube) is not None
+
+def test_global_ocean_plot():
+    ice_cube = iris.load_cube("./tests/testdata/sithic-north-mar.nc")
+    assert mth.polar_ice_sheet_plot(ice_cube) is not None
+
+def test_global_atmosphere_plot():
+    atmo_cube = iris.load_cube("./tests/testdata/tas-climatology.nc")
+    assert mth.global_atmosphere_plot(atmo_cube) is not None
+
+def test_function_mapper():
+    assert mth.function_mapper('global ocean') == mth.global_ocean_plot
+    assert mth.function_mapper('global atmosphere') == mth.global_atmosphere_plot
+    assert mth.function_mapper('polar ice sheet') == mth.polar_ice_sheet_plot
+    assert mth.function_mapper("invalid") == None
