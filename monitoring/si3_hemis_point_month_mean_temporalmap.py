48f6570b01a652ee7c79c79ff78f660da01c55b6
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-02-09 13:43:26 +0100

    Implement month opt for sea-ice temporal maps

--- monitoring/si3_hemis_point_month_mean_temporalmap.py
+++ monitoring/si3_hemis_point_month_mean_temporalmap.py
@@ -1,6 +1,7 @@
 """Processing Task that creates a 2D time map of sea ice concentration."""
 
 import datetime
+from turtle import shape
 
 import cftime
 import iris
@@ -12,16 +13,112 @@ import helpers.file_handling as helpers
 
 from .temporalmap import Temporalmap
 
-meta_dict = {
+_meta_dict = {
     "sivolu": {
         "long_name": "Sea-Ice Volume per Area",
-        "convert_to": None,
     },
     "siconc": {
         "long_name": "Sea-Ice Area Fraction",
         "convert_to": "%",
     },
 }
+
+
+def _remove_aux_time(cube):
+    try:
+        aux_time = cube.coord("time", dim_coords=False)
+    except iris.exceptions.CoordinateNotFoundError:
+        pass
+    else:
+        cube.remove_coord(aux_time)
+    return cube
+
+
+_month_names = (
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+)
+
+
+def _month_number(month):
+    "Returns number of month (1..12) for month (int or string)"
+    nums = dict()
+    # Allow all of "January", "january", "jan", 1
+    for num, name in enumerate(_month_names):
+        nums[name] = nums[num + 1] = nums[name[:3]] = nums[name.lower()] = num + 1
+    try:
+        return nums[month]
+    except KeyError:
+        raise ValueError(f"Invalid month: '{month}'")
+
+
+def _month_name(month):
+    return _month_names[_month_number(month) - 1]
+
+
+def _extract_month(cube, month):
+    return cube.extract(
+        iris.Constraint(time=lambda cell: cell.point.month == _month_number(month))
+    )
+
+
+def _mask_other_hemisphere(cube, hemisphere):
+    lats = np.broadcast_to(cube.coord("latitude").points, cube.shape)
+    if hemisphere.lower() in ("south", "s"):
+        cube.data = np.ma.masked_where(lats > 0, cube.data)
+    elif hemisphere.lower() in ("north", "n"):
+        cube.data = np.ma.masked_where(lats < 0, cube.data)
+    else:
+        raise ValueError("Invalid hemisphere, must be 'north' or 'south'")
+    return cube
+
+
+def _set_cell_methods(cube, hemisphere):
+    cube.cell_methods = ()
+    cube.add_cell_method(iris.coords.CellMethod("point", coords="time"))
+    cube.add_cell_method(
+        iris.coords.CellMethod(
+            "point",
+            coords="latitude",
+            comments=f"{hemisphere}ern hemisphere",
+        )
+    )
+    cube.add_cell_method(iris.coords.CellMethod("point", coords="longitude"))
+    return cube
+
+
+def _yearly_time_bounds(cube):
+
+    t_coord = cube.coord("time")
+    cube.remove_coord("time")
+
+    t_point = t_coord.units.num2date(t_coord.points[0])
+    t_first, t_last = (
+        t_coord.units.date2num(t_point.replace(month=1, day=1)),
+        t_coord.units.date2num(t_point.replace(year=t_point.year + 1, month=1, day=1)),
+    )
+    cube.add_aux_coord(
+        iris.coords.DimCoord(
+            t_coord.points,
+            bounds=np.array([[t_first, t_last]]),
+            standard_name=t_coord.standard_name,
+            long_name=t_coord.long_name,
+            units=t_coord.units,
+            var_name=t_coord.var_name,
+            attributes=t_coord.attributes,
+        )
+    )
+    return iris.util.new_axis(cube, "time")
 
 
 class Si3HemisPointMonthMeanTemporalmap(Temporalmap):
@@ -44,99 +141,64 @@ class Si3HemisPointMonthMeanTemporalmap(Temporalmap):
         dst = self.getarg("dst", context)
         hemisphere = self.getarg("hemisphere", context)
         varname = self.getarg("varname", context)
+        month = self.getarg("month", context, default=False)
 
         self.log_info(
-            f"Temporal map for {varname} on {hemisphere}ern hemisphere at {dst}."
+            f"Temporal map for {varname} on {hemisphere}ern hemisphere at {dst}"
         )
         self.log_debug(f"Source file(s): {src}")
 
-        if varname not in meta_dict:
-            msg = (
-                f"'varname' must be one of the following: {meta_dict.keys()} "
-                "Diagnostic will not be treated, returning now."
+        if varname not in _meta_dict:
+            self.log_warning(
+                (
+                    f"Invalid varname argument: '{varname}', must be one of {_meta_dict.keys()}."
+                    " Diagnostic will not be included!"
+                )
             )
-            self.log_error(msg)
-            raise ScriptEngineTaskArgumentInvalidError()
-        if not hemisphere in ("north", "south"):
-            msg = (
-                f"Invalid hemisphere argument '{hemisphere}', must be 'north' or 'south'."
-                " Diagnostic will not be included!"
+            return
+        if hemisphere not in ("north", "south"):
+            self.log_warning(
+                (
+                    f"Invalid hemisphere argument '{hemisphere}', must be 'north' or 'south'."
+                    " Diagnostic will not be included!"
+                )
             )
-            self.log_error(msg)
-            raise ScriptEngineTaskArgumentInvalidError()
+            return
         self.check_file_extension(dst)
 
-        month_cube = helpers.load_input_cube(src, varname)
-        # Remove auxiliary time coordinate
-        month_cube.remove_coord(month_cube.coord("time", dim_coords=False))
+        this_leg = helpers.load_input_cube(src, varname)
+        this_leg = _remove_aux_time(this_leg)
+        this_leg = _mask_other_hemisphere(this_leg, hemisphere)
+        if month:
+            this_leg = _extract_month(this_leg, month)
+            this_leg = _yearly_time_bounds(this_leg)
 
-        time_coord = month_cube.coord("time")
-        time_coord.bounds = self.get_time_bounds(time_coord)
-        month_cube = self.mask_irrelevant_cells(month_cube, hemisphere)
-        if meta_dict[varname]["convert_to"] is not None:
-            month_cube.convert_units(meta_dict[varname]["convert_to"])
+        if "convert_to" in _meta_dict[varname]:
+            try:
+                this_leg.convert_units(_meta_dict[varname]["convert_to"])
+            except iris.exceptions.UnitConversionError:
+                self.log_warning(
+                    f"Cannot convert unit '{this_leg.units}' to '{_meta_dict[varname]['convert_to']}'"
+                )
 
-        month_cube.long_name = f"{meta_dict[varname]['long_name']} {hemisphere}"
-        comment = f"{meta_dict[varname]['long_name']} / **{varname}** on {hemisphere}ern hemisphere."
-        month_cube = helpers.set_metadata(
-            month_cube,
-            title=f"{month_cube.long_name} {self.get_month(time_coord)}",
+        long_name = (
+            _meta_dict[varname]["long_name"]
+            + " "
+            + hemisphere
+            + (f" {_month_name(month)}" if month else "")
+        )
+        comment = (
+            f"{_meta_dict[varname]['long_name']} / **{varname}** in {hemisphere}ern hemisphere"
+            + (f" in {_month_name(month)}" if month else "")
+        )
+        this_leg.long_name = long_name
+        this_leg = helpers.set_metadata(
+            this_leg,
+            title=long_name,
             comment=comment,
             map_type="polar ice sheet",
         )
 
-        month_cube = self.set_cell_methods(month_cube, hemisphere)
+        this_leg = _set_cell_methods(this_leg, hemisphere)
 
-        self.save(month_cube, dst)
-
-    def mask_irrelevant_cells(self, month_cube, hemisphere):
-        """
-        Mask grid cells without ice and on the 'other' hemisphere.
-        """
-        latitudes = np.broadcast_to(
-            month_cube.coord("latitude").points, month_cube.shape
-        )
-        if hemisphere == "north":
-            month_cube.data = np.ma.masked_where(latitudes < 0, month_cube.data)
-        else:
-            month_cube.data = np.ma.masked_where(latitudes > 0, month_cube.data)
-        month_cube.data = np.ma.masked_equal(month_cube.data, 0)
-        return month_cube
-
-    def get_time_bounds(self, time_coord):
-        """
-        Get contiguous time bounds for sea ice maps
-
-        Creates new time bounds [
-            [01-01-current year, 01-01-next year],
-        ]
-        """
-        dt_object = cftime.num2pydate(time_coord.points[0], time_coord.units.name)
-        start = datetime.datetime(dt_object.year, 1, 1)
-        end = datetime.datetime(start.year + 1, 1, 1)
-        start_seconds = cftime.date2num(start, time_coord.units.name)
-        end_seconds = cftime.date2num(end, time_coord.units.name)
-        new_bounds = np.array([[start_seconds, end_seconds]])
-        return new_bounds
-
-    def get_month(self, time_coord):
-        """
-        Returns the month of time_coord.points[0] as a string
-        """
-        dt_object = cftime.num2pydate(time_coord.points[0], time_coord.units.name)
-        return dt_object.strftime("%B")
-
-    def set_cell_methods(self, cube, hemisphere):
-        """Set the correct cell methods."""
-        self.log_debug("Setting cell methods.")
-        cube.cell_methods = ()
-        cube.add_cell_method(iris.coords.CellMethod("point", coords="time"))
-        cube.add_cell_method(
-            iris.coords.CellMethod(
-                "point",
-                coords="latitude",
-                comments=f"{hemisphere}ern hemisphere",
-            )
-        )
-        cube.add_cell_method(iris.coords.CellMethod("point", coords="longitude"))
-        return cube
+        self.save(this_leg, dst)
