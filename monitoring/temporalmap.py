9f6322f7cfa46fc40d257e07e27adad0be14b236
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-02-10 09:21:06 +0100

    isort+black for Timeseries and Temporalmap classes

--- monitoring/temporalmap.py
+++ monitoring/temporalmap.py
@@ -3,33 +3,35 @@
 import os
 
 import iris
-
 from scriptengine.exceptions import ScriptEngineTaskArgumentInvalidError
 from scriptengine.tasks.core import Task
+
 
 class Temporalmap(Task):
     """Temporalmap Processing Task"""
 
     def run(self, context):
-        raise NotImplementedError('Base class function Temporalmap.run() must not be called')
+        raise NotImplementedError(
+            "Base class function Temporalmap.run() must not be called"
+        )
 
     def save(self, new_cube, dst):
         """save temporal map cube in netCDF file"""
         self.log_debug(f"Saving temporal map cube to {dst}")
-        new_cube.attributes['diagnostic_type'] = 'temporal map'
+        new_cube.attributes["diagnostic_type"] = "temporal map"
         try:
             current_cube = iris.load_cube(dst)
-        except OSError: # file does not exist yet.
+        except OSError:  # file does not exist yet.
             iris.save(new_cube, dst)
             return
 
         # Iris changes metadata when saving/loading cube
         # save & reload to prevent metadata mismatch
-        iris.save(new_cube, 'temp.nc')
-        new_cube = iris.load_cube('temp.nc')
+        iris.save(new_cube, "temp.nc")
+        new_cube = iris.load_cube("temp.nc")
 
-        current_bounds = current_cube.coord('time').bounds
-        new_bounds = new_cube.coord('time').bounds
+        current_bounds = current_cube.coord("time").bounds
+        new_bounds = new_cube.coord("time").bounds
         if not current_bounds[-1][-1] > new_bounds[0][0]:
             new_cube.attributes = current_cube.attributes
             cube_list = iris.cube.CubeList([current_cube, new_cube])
@@ -40,14 +42,10 @@ class Temporalmap(Task):
         else:
             self.log_warning("Non-monotonic coordinate. Cube will not be saved.")
         # remove temporary save
-        os.remove('temp.nc')
+        os.remove("temp.nc")
 
     def check_file_extension(self, dst):
         """check if destination file has a valid netCDF extension"""
         if not dst.endswith(".nc"):
-            msg = (
-                f"{dst} does not end in valid netCDF file extension. "
-                f"Diagnostic will not be treated, returning now."
-            )
-            self.log_error(msg)
+            self.log_error(f"Invalid netCDF extension in dst '{dst}'")
             raise ScriptEngineTaskArgumentInvalidError()
--- monitoring/timeseries.py
+++ monitoring/timeseries.py
@@ -1,20 +1,23 @@
 """Processing Task that writes out a generalized time series diagnostic."""
 
-import os
 import datetime
+import os
 
 import iris
 import numpy as np
-
+from scriptengine.exceptions import (
+    ScriptEngineTaskArgumentInvalidError,
+    ScriptEngineTaskRunError,
+)
 from scriptengine.tasks.core import Task, timed_runner
-from scriptengine.exceptions import ScriptEngineTaskArgumentInvalidError, ScriptEngineTaskRunError
+
 import helpers.file_handling as helpers
 
 
 class Timeseries(Task):
     """Processing Task that writes out a generalized time series diagnostic."""
 
-    _required_arguments = ('title', 'coord_value', 'data_value', 'dst')
+    _required_arguments = ("title", "coord_value", "data_value", "dst")
 
     def __init__(self, arguments=None):
         Timeseries.check_arguments(arguments)
@@ -23,28 +26,30 @@ class Timeseries(Task):
     @timed_runner
     def run(self, context):
         # load input parameters
-        title = self.getarg('title', context)
-        dst = self.getarg('dst', context)
+        title = self.getarg("title", context)
+        dst = self.getarg("dst", context)
         self.log_info(f"Time series {title} at {dst}.")
 
         # Convert coordinate value to number and get unit
         coord_value = self.getarg("coord_value", context, default=None)
         if type(coord_value) in (datetime.datetime, datetime.date):
             try:
-                coord_value = (coord_value - datetime.datetime(1900, 1, 1)).total_seconds()
+                coord_value = (
+                    coord_value - datetime.datetime(1900, 1, 1)
+                ).total_seconds()
             except TypeError:
                 coord_value = (coord_value - datetime.date(1900, 1, 1)).total_seconds()
             coord_unit = "second since 1900-01-01 00:00:00"
         else:
             coord_unit = self.getarg("coord_unit", context, default="1")
-        
+
         data_value = self.getarg("data_value", context)
         data_unit = self.getarg("data_unit", context, default="1")
 
-        coord_name = self.getarg('coord_name', context, default='time')
-        data_name = self.getarg('data_name', context, default=title)
-        comment = self.getarg('comment', context, default=".")
-        
+        coord_name = self.getarg("coord_name", context, default="time")
+        data_name = self.getarg("data_name", context, default=title)
+        comment = self.getarg("comment", context, default=".")
+
         self.log_debug(f"Value: {data_value} at time: {coord_value}, title: {title}")
 
         self.check_file_extension(dst)
@@ -78,10 +83,10 @@ class Timeseries(Task):
         """save time series cube in netCDF file"""
         self.log_debug(f"Saving time series cube to {dst}")
 
-        new_cube.attributes['diagnostic_type'] = 'time series'
+        new_cube.attributes["diagnostic_type"] = "time series"
         try:
             current_cube = iris.load_cube(dst)
-        except OSError: # file does not exist yet.
+        except OSError:  # file does not exist yet.
             iris.save(new_cube, dst)
             return
 
@@ -89,8 +94,8 @@ class Timeseries(Task):
 
         # Iris changes metadata when saving/loading cube
         # save & reload to prevent metadata mismatch
-        iris.save(new_cube, 'temp.nc')
-        new_cube = iris.load_cube('temp.nc')
+        iris.save(new_cube, "temp.nc")
+        new_cube = iris.load_cube("temp.nc")
 
         cube_list = iris.cube.CubeList([current_cube, new_cube])
         merged_cube = cube_list.concatenate_cube()
@@ -99,7 +104,7 @@ class Timeseries(Task):
         os.remove(dst)
         os.rename(f"{dst}-copy.nc", dst)
         # remove temporary save
-        os.remove('temp.nc')
+        os.remove("temp.nc")
 
     def test_monotonic_increase(self, old_coord, new_coord):
         """Test if coordinate is monotonically increasing."""
@@ -117,9 +122,5 @@ class Timeseries(Task):
     def check_file_extension(self, dst):
         """check if destination file has a valid netCDF extension"""
         if not dst.endswith(".nc"):
-            msg = (
-                f"{dst} does not end in valid netCDF file extension. "
-                f"Diagnostic will not be treated, returning now."
-            )
-            self.log_error(msg)
+            self.log_error(f"Invalid netCDF extension in dst '{dst}'")
             raise ScriptEngineTaskArgumentInvalidError()
