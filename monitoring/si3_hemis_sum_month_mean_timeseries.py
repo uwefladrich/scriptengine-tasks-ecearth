890882e2cd46adff39e377ff4e849109fb85b1c5
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-02-16 10:50:41 +0100

    isort+black for SI3 diags and tests

--- monitoring/si3_hemis_sum_month_mean_timeseries.py
+++ monitoring/si3_hemis_sum_month_mean_timeseries.py
@@ -2,74 +2,88 @@
 
 import warnings
 
+import cf_units
 import iris
 import numpy as np
-import cf_units
-
 from scriptengine.tasks.core import timed_runner
+
 import helpers.file_handling as helpers
+
 from .timeseries import Timeseries
 
 meta_dict = {
-    'sivolu':
-        {
-            'long_name': 'Sea-Ice Volume',
-            'standard_name': 'sea_ice_volume',
-            'var_name': 'sivol',
-            'old_unit': 'm3',
-            'new_unit': '1e3 km3',
-        },
-    'siconc':
-        {
-            'long_name': 'Sea-Ice Area',
-            'standard_name': 'sea_ice_area',
-            'var_name': 'siarea',
-            'old_unit': 'm2',
-            'new_unit': '1e6 km2',
-        },
+    "sivolu": {
+        "long_name": "Sea-Ice Volume",
+        "standard_name": "sea_ice_volume",
+        "var_name": "sivol",
+        "old_unit": "m3",
+        "new_unit": "1e3 km3",
+    },
+    "siconc": {
+        "long_name": "Sea-Ice Area",
+        "standard_name": "sea_ice_area",
+        "var_name": "siarea",
+        "old_unit": "m2",
+        "new_unit": "1e6 km2",
+    },
 }
+
 
 class Si3HemisSumMonthMeanTimeseries(Timeseries):
     """Si3HemisSumMonthMeanTimeseries Processing Task"""
 
-    _required_arguments = ('src', 'domain', 'hemisphere', 'varname', 'dst', )
+    _required_arguments = (
+        "src",
+        "domain",
+        "hemisphere",
+        "varname",
+        "dst",
+    )
 
     def __init__(self, arguments):
         Si3HemisSumMonthMeanTimeseries.check_arguments(arguments)
-        super().__init__({**arguments, 'title': None, 'coord_value': None, 'data_value': None})
+        super().__init__(
+            {**arguments, "title": None, "coord_value": None, "data_value": None}
+        )
 
     @timed_runner
     def run(self, context):
-        dst = self.getarg('dst', context)
-        varname = self.getarg('varname', context)
-        hemisphere = self.getarg('hemisphere', context)
+        dst = self.getarg("dst", context)
+        varname = self.getarg("varname", context)
+        hemisphere = self.getarg("hemisphere", context)
 
-        self.log_info(f"Create {varname} time series for {hemisphere}ern hemisphere at {dst}.")
+        self.log_info(
+            f"Create {varname} time series for {hemisphere}ern hemisphere at {dst}."
+        )
 
         if varname not in meta_dict:
-            self.log_warning((
-                f"'varname' must be one of the following: {meta_dict.keys()} "
-                f"Diagnostic will not be treated, returning now."
-            ))
+            self.log_warning(
+                (
+                    f"'varname' must be one of the following: {meta_dict.keys()} "
+                    "Diagnostic will not be treated, returning now."
+                )
+            )
             return
-        long_name = meta_dict[varname]['long_name']
+        long_name = meta_dict[varname]["long_name"]
 
-        src = self.getarg('src', context)
-        domain = self.getarg('domain', context)
-        
+        src = self.getarg("src", context)
+        domain = self.getarg("domain", context)
+
         self.log_debug(f"Domain: {domain}, Source file(s): {src}")
 
-        if not hemisphere in ('north', 'south'):
-            self.log_warning((
-                f"'hemisphere' must be 'north' or 'south' but is '{hemisphere}'."
-                f"Diagnostic will not be treated, returning now."
-            ))
+        if not hemisphere in ("north", "south"):
+            self.log_warning(
+                (
+                    f"'hemisphere' must be 'north' or 'south' but is '{hemisphere}'."
+                    "Diagnostic will not be treated, returning now."
+                )
+            )
             return
         self.check_file_extension(dst)
 
         leg_cube = helpers.load_input_cube(src, varname)
-        cell_weights = helpers.compute_spatial_weights(domain, leg_cube.shape, 'T')
-        latitudes = np.broadcast_to(leg_cube.coord('latitude').points, leg_cube.shape)
+        cell_weights = helpers.compute_spatial_weights(domain, leg_cube.shape, "T")
+        latitudes = np.broadcast_to(leg_cube.coord("latitude").points, leg_cube.shape)
         if hemisphere == "north":
             leg_cube.data = np.ma.masked_where(latitudes < 0, leg_cube.data)
         else:
@@ -77,37 +91,41 @@ class Si3HemisSumMonthMeanTimeseries(Timeseries):
         with warnings.catch_warnings():
             # Suppress warning about insufficient metadata.
             warnings.filterwarnings(
-                'ignore',
+                "ignore",
                 "Collapsing a multi-dimensional coordinate.",
                 UserWarning,
-                )
+            )
             hemispheric_sum = leg_cube.collapsed(
-                ['latitude', 'longitude'],
+                ["latitude", "longitude"],
                 iris.analysis.SUM,
                 weights=cell_weights,
-                )
+            )
 
         # Remove auxiliary time coordinate
-        hemispheric_sum.remove_coord(hemispheric_sum.coord('time', dim_coords=False))
-        hemispheric_sum.standard_name = meta_dict[varname]['standard_name']
-        hemispheric_sum.units = cf_units.Unit(meta_dict[varname]['old_unit'])
-        hemispheric_sum.convert_units(meta_dict[varname]['new_unit'])
+        hemispheric_sum.remove_coord(hemispheric_sum.coord("time", dim_coords=False))
+        hemispheric_sum.standard_name = meta_dict[varname]["standard_name"]
+        hemispheric_sum.units = cf_units.Unit(meta_dict[varname]["old_unit"])
+        hemispheric_sum.convert_units(meta_dict[varname]["new_unit"])
         hemispheric_sum.long_name = f"{long_name} {hemisphere.capitalize()}"
-        hemispheric_sum.var_name = meta_dict[varname]['var_name'] + hemisphere[0]
+        hemispheric_sum.var_name = meta_dict[varname]["var_name"] + hemisphere[0]
 
         metadata = {
-            'comment': (f"Sum of {long_name} / **{varname}** on {hemisphere}ern hemisphere."),
-            'title': f"{long_name} (Seasonal Cycle)",
+            "comment": (
+                f"Sum of {long_name} / **{varname}** on {hemisphere}ern hemisphere."
+            ),
+            "title": f"{long_name} (Seasonal Cycle)",
         }
         hemispheric_sum = helpers.set_metadata(hemispheric_sum, **metadata)
         hemispheric_sum = self.set_cell_methods(hemispheric_sum, hemisphere)
         self.save(hemispheric_sum, dst)
-    
+
     def set_cell_methods(self, cube, hemisphere):
         """Set the correct cell methods."""
         cube.cell_methods = ()
-        cube.add_cell_method(iris.coords.CellMethod('point', coords='time'))
+        cube.add_cell_method(iris.coords.CellMethod("point", coords="time"))
         cube.add_cell_method(
-            iris.coords.CellMethod('sum', coords='area', intervals=f'{hemisphere}ern hemisphere')
+            iris.coords.CellMethod(
+                "sum", coords="area", intervals=f"{hemisphere}ern hemisphere"
             )
+        )
         return cube
--- tests/test_si3_hemis_point_month_mean_temporalmap.py
+++ tests/test_si3_hemis_point_month_mean_temporalmap.py
@@ -2,48 +2,53 @@
 
 import iris
 import pytest
-
 import scriptengine.exceptions
-from monitoring.si3_hemis_point_month_mean_temporalmap import Si3HemisPointMonthMeanTemporalmap
+
+from monitoring.si3_hemis_point_month_mean_temporalmap import (
+    Si3HemisPointMonthMeanTemporalmap,
+)
+
 
 def test_si3_hemis_point_month_mean_temporalmap_once(tmpdir):
     init = {
-        "src": './tests/testdata/NEMO_output_sivolu-199003.nc',
-        "dst": str(tmpdir) + '/test.nc',
+        "src": "./tests/testdata/NEMO_output_sivolu-199003.nc",
+        "dst": str(tmpdir) + "/test.nc",
         "hemisphere": "north",
         "varname": "sivolu",
     }
     ice_time_map = Si3HemisPointMonthMeanTemporalmap(init)
     ice_time_map.run(init)
-    cube = iris.load_cube(init['dst'])
-    assert cube.attributes['map_type'] == 'polar ice sheet'
-    assert cube.attributes['diagnostic_type'] == 'temporal map'
+    cube = iris.load_cube(init["dst"])
+    assert cube.attributes["map_type"] == "polar ice sheet"
+    assert cube.attributes["diagnostic_type"] == "temporal map"
+
 
 def test_si3_hemis_point_month_mean_temporalmap_twice(tmpdir):
     init_a = {
-        "src": './tests/testdata/NEMO_output_sivolu-199003.nc',
-        "dst": str(tmpdir) + '/test.nc',
+        "src": "./tests/testdata/NEMO_output_sivolu-199003.nc",
+        "dst": str(tmpdir) + "/test.nc",
         "hemisphere": "north",
         "varname": "sivolu",
     }
     ice_time_map = Si3HemisPointMonthMeanTemporalmap(init_a)
     ice_time_map.run(init_a)
     init_b = {
-        "src": './tests/testdata/NEMO_output_sivolu-199103.nc',
-        "dst": str(tmpdir) + '/test.nc',
+        "src": "./tests/testdata/NEMO_output_sivolu-199103.nc",
+        "dst": str(tmpdir) + "/test.nc",
         "hemisphere": "north",
         "varname": "sivolu",
     }
     ice_time_map = Si3HemisPointMonthMeanTemporalmap(init_b)
     ice_time_map.run(init_b)
-    cube = iris.load_cube(init_b['dst'])
-    assert cube.attributes['map_type'] == 'polar ice sheet'
-    assert cube.attributes['diagnostic_type'] == 'temporal map'
+    cube = iris.load_cube(init_b["dst"])
+    assert cube.attributes["map_type"] == "polar ice sheet"
+    assert cube.attributes["diagnostic_type"] == "temporal map"
+
 
 def test_si3_hemis_point_month_mean_temporalmap_wrong_varname(tmpdir):
     init = {
-        "src": './tests/testdata/NEMO_output_sivolu-199003.nc',
-        "dst": str(tmpdir) + '/test.nc',
+        "src": "./tests/testdata/NEMO_output_sivolu-199003.nc",
+        "dst": str(tmpdir) + "/test.nc",
         "hemisphere": "north",
         "varname": "tos",
     }
@@ -54,10 +59,11 @@ def test_si3_hemis_point_month_mean_temporalmap_wrong_varname(tmpdir):
         init,
     )
 
+
 def test_si3_hemis_point_month_mean_temporalmap_wrong_hemisphere(tmpdir):
     init = {
-        "src": './tests/testdata/NEMO_output_sivolu-199003.nc',
-        "dst": str(tmpdir) + '/test.nc',
+        "src": "./tests/testdata/NEMO_output_sivolu-199003.nc",
+        "dst": str(tmpdir) + "/test.nc",
         "varname": "sivolu",
         "hemisphere": "east",
     }
@@ -67,5 +73,6 @@ def test_si3_hemis_point_month_mean_temporalmap_wrong_hemisphere(tmpdir):
         ice_time_map.run,
         init,
     )
+
 
 # missing: 'convert_to' test (requires a siconc input file) or multiple Mocks
