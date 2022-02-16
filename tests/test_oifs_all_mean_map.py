dfe6aa4492bcb94b51d0f8f89f6f6369f5905f25
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-02-16 09:50:58 +0100

    isort+black for OIFS diags and tests

--- tests/test_oifs_all_mean_map.py
+++ tests/test_oifs_all_mean_map.py
@@ -2,37 +2,37 @@
 
 import iris
 import pytest
-
 import scriptengine.exceptions
+
 from monitoring.oifs_all_mean_map import OifsAllMeanMap
+
 
 def test_oifs_all_mean_map_working(tmpdir):
     init = {
-        "src": ['./tests/testdata/TES1_atm_1m_1990_2t.nc'],
-        "dst": str(tmpdir) + '/test.nc',
+        "src": ["./tests/testdata/TES1_atm_1m_1990_2t.nc"],
+        "dst": str(tmpdir) + "/test.nc",
         "varname": "2t",
     }
     atmo_map = OifsAllMeanMap(init)
     atmo_map.run(init)
-    cube = iris.load_cube(str(tmpdir) + '/test.nc')
+    cube = iris.load_cube(str(tmpdir) + "/test.nc")
     assert cube.name() == "2 metre temperature"
-    assert cube.attributes['title'] is not None
-    assert cube.attributes['comment'] is not None
-    assert cube.attributes['diagnostic_type'] == 'map'
-    assert cube.attributes['map_type'] == 'global atmosphere'
-    assert cube.coord('time').climatological
-    assert len(cube.coord('time').points) == 1
+    assert cube.attributes["title"] is not None
+    assert cube.attributes["comment"] is not None
+    assert cube.attributes["diagnostic_type"] == "map"
+    assert cube.attributes["map_type"] == "global atmosphere"
+    assert cube.coord("time").climatological
+    assert len(cube.coord("time").points) == 1
+
 
 def test_oifs_all_mean_map_wrong_code(tmpdir):
     init = {
-        "src": ['./tests/testdata/TES1_atm_1m_1990_2t.nc'],
-        "dst": str(tmpdir) + '/test.nc',
+        "src": ["./tests/testdata/TES1_atm_1m_1990_2t.nc"],
+        "dst": str(tmpdir) + "/test.nc",
         "varname": "sivolu",
     }
     atmo_map = OifsAllMeanMap(init)
 
     pytest.raises(
-        scriptengine.exceptions.ScriptEngineTaskArgumentInvalidError,
-        atmo_map.run,
-        init
+        scriptengine.exceptions.ScriptEngineTaskArgumentInvalidError, atmo_map.run, init
     )
