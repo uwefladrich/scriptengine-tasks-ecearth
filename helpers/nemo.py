f553c658a57133e53947f7cff4561db6336bbfe7
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-03-04 11:52:24 +0100

    Extend spatial_weights for 3d case

--- helpers/nemo.py
+++ helpers/nemo.py
@@ -3,17 +3,41 @@
 import iris
 import numpy as np
 
+_nemo_horizontal_coords = (
+    "latitude",
+    "longitude",
+)
 
-def spatial_weights(domain_file, array_shape, grid):
-    "Compute weights for spatial averaging"
+_nemo_vertical_coords = (
+    "Vertical T levels",
+    "Vertical U levels",
+    "Vertical V levels",
+)
+
+
+def spatial_coords(cube):
+    """Returns the names of all cube coordinates that are listed as 'spatial' coordinates"""
+    for cname in (c.name() for c in cube.coords()):
+        if cname in (*_nemo_horizontal_coords, *_nemo_vertical_coords):
+            yield cname
+
+
+def _has_vertical_coord(cube):
+    return not set(spatial_coords(cube)).isdisjoint(set(_nemo_vertical_coords))
+
+
+def spatial_weights(cube, domain_file, grid):
+    """Compute cell weights for spatial averaging in 2d and 3d"""
     domain = iris.load(domain_file)
     e1, e2 = (  # NEMO grid scale factors in horizontal directions
         domain.extract(f"e1{grid.lower()}")[0][0],
         domain.extract(f"e2{grid.lower()}")[0][0],
     )
-    cell_areas = e1 * e2
-    cell_weights = np.broadcast_to(cell_areas.data, array_shape)
-    return cell_weights
+    is_3d = _has_vertical_coord(cube)
+    if is_3d:
+        e3 = domain.extract(f"e3{grid.lower()}_0")[0][0]
+    weights = e1 * e2 * e3 if is_3d else e1 * e2
+    return np.broadcast_to(weights.data, cube.shape)
 
 
 def remove_unique_attributes(cube):
