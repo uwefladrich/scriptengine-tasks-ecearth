917f89c476eb0d93e742b3e8c45bf26e64cec34b
Klaus Wyser <klaus.wyser@smhi.se>
2024-10-21 15:41:50 +0200

    change units for precipitation, evaporation and P-E to mm/day

--- monitoring/oifs_global_mean_year_mean_timeseries.py
+++ monitoring/oifs_global_mean_year_mean_timeseries.py
@@ -131,4 +131,7 @@ class OifsGlobalMeanYearMeanTimeseries(Timeseries):
         # Convert unit to °C if varname is given in K
         if timeseries_cube.units.name == "kelvin":
             timeseries_cube.convert_units("degC")
+        # Convert unit to mm/day if varname is given in kg m-2 s-1
+        if timeseries_cube.units.name == "meter^-2-kilogram-second^-1":
+            timeseries_cube.convert_units("meter^-2-kilogram-day^-1")
         return timeseries_cube
