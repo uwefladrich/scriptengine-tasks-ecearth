add80dbdfafd422b23408fa5b00c0a8b66cffede
Valentina Schüller <valentina.schuller@math.lu.se>
2025-02-19 12:55:59 +0100

    Update test to use scalar output file

--- tests/test_nemo_timeseries.py
+++ tests/test_nemo_timeseries.py
@@ -84,34 +84,16 @@ def test_nemo_year_mean_timeseries_wrong_dim(tmp_path):
 
 
 def test_nemo_year_mean_timeseries_working(tmp_path):
-    dim_coord = iris.coords.DimCoord(
-        [1.0, 2.0],
-        bounds=[[0.5, 1.5], [1.5, 2.5]],
-        standard_name="time",
-        var_name="time",
-    )
-    aux_coord = iris.coords.AuxCoord([1, 2], standard_name="time", var_name="aux_time")
-    cube = iris.cube.Cube(
-        [0.0, 1.0],
-        dim_coords_and_dims=[(dim_coord, 0)],
-        aux_coords_and_dims=[(aux_coord, 0)],
-        var_name="data",
-    )
-    in_cube_path = tmp_path / "test_in.nc"
-    iris.save(cube, in_cube_path)
     init = {
-        "src": [in_cube_path],
-        "dst": str(tmp_path / "test_out.nc"),
-        "varname": "data",
+        "src": ["./tests/testdata/a8gx_bgc_5d_bioscalar_1990-1990.nc"],
+        "dst": str(tmp_path / "test.nc"),
+        "varname": "tdenit",
     }
     year_mean_ts = NemoYearMeanTimeseries(init)
     year_mean_ts.run(init)
-
     out_cube = iris.load_cube(init["dst"])
     assert out_cube.shape == (1,)
     assert out_cube.coord().has_bounds()
-    assert np.all(
-        out_cube.coord().bounds
-        == np.array([dim_coord.bounds[0, 0], dim_coord.bounds[-1, -1]])
+    assert out_cube.cell_methods == (
+        iris.coords.CellMethod("mean", coords="time", intervals="1 year"),
     )
-    assert out_cube.coord().points[0] == np.mean(dim_coord.points)
