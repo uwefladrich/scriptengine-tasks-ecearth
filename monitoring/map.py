bfc0df135076f81ef6c9f99e832c0e6ad0dbfe56
Joakim Kjellsson <gdjk@ac6-102.bullx>
2026-02-16 09:13:45 +0000

    align time coords in monitoring/map.py

--- monitoring/map.py
+++ monitoring/map.py
@@ -26,22 +26,9 @@ class Map(Task):
             iris.save(new_cube, str(dst))
             return
         
-        # The rollback function in EC-Earth will reset the initial time
-        # for OpenIFS each restart.
-        # If we start in 1990-01-01, the time coordinate for 1st and 2nd
-        # leg will be 1990-01-01 but it will be 1991-01-01 for 3rd leg
-        # and 1992-01-01 for 4th leg etc.
-        # Iris will not be able to merge the cubes.
-        # Here we convert the time coordinate in new_cube, e.g.
-        # seconds since 1992-01-01 00:00:00
-        # to the time coordinate of the current_cube e.g.
-        # seconds since 1990-01-01 00:00:00
-        new_cube.coord("time").convert_units(current_cube.coord("time").units)
-        # We also need to match the attribute time_origin between
-        # new_cube and current_cube to make Iris happy.
-        if (     "time_origin" in current_cube.coord("time").attributes.keys()
-             and "time_origin" in     new_cube.coord("time").attributes.keys() ):
-            new_cube.coord("time").attributes["time_origin"] = current_cube.coord("time").attributes["time_origin"]
+        # align the time coordinates of current and new cube.
+        # Sets units and coordinate attributes to be the same
+        new_cube = helpers.cubes.align_time_coords(new_cube, current_cube)
         
         current_bounds = current_cube.coord("time").bounds
         new_bounds = new_cube.coord("time").bounds
